8db0f8e6569ef49f0efa55cd8887db460cab0073
VicChan <victorchanchina@gmail.com>
2016-06-23 17:52:17 +0800

    add Filter, Generics

--- Functional_Swift_Filter_Map_Reduce.playground/Contents.swift
+++ Functional_Swift_Filter_Map_Reduce.playground/Contents.swift
@@ -6,7 +6,7 @@ import UIKit
 
 
 
-// MARK: Filter a kind of function wrapper
+// MARK: Filter a kind of function wrapper which works on every element
 // MARK: an introduction to CIImage
 
 typealias Filter = CIImage -> CIImage
@@ -40,5 +40,92 @@ func blur(radius: Double) -> Filter {
     
     }
 }
+/*
+let image = CIImage()
+
+let ciimage = blur(20)(image)
+
+ */
+
+// Or
+/*
+let imageBlur = blur(20)
+let image = CIImage()
+let ciimage = imageBlur(image)
+
+*/
+
+
+//: Color Overlay
+
+func colorGenerator(color: CIColor) -> Filter {
+    return { _ in
+        let parameters = [kCIInputColorKey: color]
+        guard let filter = CIFilter(name:"kCIConstantColorGenerator", withInputParameters: parameters) else {
+            fatalError()
+        }
+        guard let outImage = filter.outputImage else {
+            fatalError()
+        }
+        return outImage
+    }
+}
+
+// Function Composing
+
+func composeFilter(filter: Filter, _ filter2: Filter) -> Filter {
+    return {
+        image in
+        filter(filter2(image))
+    }
+}
+
+
+
+// MARK: Genetics <>
+
+// create a stack first
+
+/*
+class Stack {
+    
+    
+    func push(num: Int) {
+        
+    }
+    
+    func pop() -> Int? {
+       return 0
+    }
+    
+    func isEmpty() -> Bool {
+        return true
+    }
+    
+    
+}
+*/
+
+/*
+class Stack<T>{
+
+    func pop() -> T? {
+        return nil
+    }
+    func push(num: T)  {
+        
+    }
+    func isEmpty() -> Bool{
+        return true
+    }
+    
+    
+}
+
+*/
+
+
+
+
 
 
