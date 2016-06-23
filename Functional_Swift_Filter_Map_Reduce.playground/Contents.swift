49f5cb234c7a610f51b055a9ad868655986613bb
VicChan <victorchanchina@gmail.com>
2016-06-23 17:10:31 +0800

    some filters

--- Functional_Swift_Filter_Map_Reduce.playground/Contents.swift
+++ Functional_Swift_Filter_Map_Reduce.playground/Contents.swift
@@ -2,5 +2,43 @@
 
 import UIKit
 
-// MARK: Filter
+// MARK: In this chapter we will know filter, map, reduce
+
+
+
+// MARK: Filter a kind of function wrapper
+// MARK: an introduction to CIImage
+
+typealias Filter = CIImage -> CIImage
+
+
+// Example
+
+/*
+func myFilter() -> Filter {
+    
+}
+
+*/
+
+// First Example Gaussian Blur
+
+func blur(radius: Double) -> Filter {
+    return {
+        image in
+        let parameters = [
+            kCIInputRadiusKey: radius,
+            kCIInputImageKey: image
+        ]
+        guard let filter = CIFilter(name: "CIGaussianBlur", withInputParameters: parameters) else {
+            fatalError()
+        }
+        guard let outImage = filter.outputImage else {
+            fatalError()
+        }
+        return outImage
+    
+    }
+}
+
 
